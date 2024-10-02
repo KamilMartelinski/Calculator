c43b2a349c7e47783b41d2938ef1f98c9667037a
Kamil Martelinski <KamilMartelinski@proton.me>
2024-10-02 15:21:11 +0200

    added comments.

--- fibonacci_sequence.cpp
+++ fibonacci_sequence.cpp
@@ -5,14 +5,14 @@ int main(){
     std::cout << "[===================================================================]" << '\n';
 
     int iterations;
-
+    //getting user input
     std::cout << "how many iterations do you want to go thro: ";
     std::cin >> iterations;
 
     int first = 1;
     int secund = 0;
     int temp = 0;
-
+    //edge cases
     if(iterations >= 1){
         std::cout << "1." <<secund << '\n';
     }
@@ -20,13 +20,13 @@ int main(){
         std::cout << "2." << first << '\n';
     }
 
-    if (iterations >= 3){
+    if (iterations >= 3){//getting output
 
         for(int i = 3; i <= iterations; i++){
             temp = first;
             first = first + secund;
             secund = temp;
-            std::cout << i << "." << first << "\n";
+            std::cout << i << "." << first << "\n";//output
         }
     }
 
--- find_every_divisible_number_in_range.cpp.cpp
+++ find_every_divisible_number_in_range.cpp.cpp
@@ -7,25 +7,25 @@ int main(){
     int divider;
 
     std::cout << "[===================================================================]" << '\n';
-
+    //getting user input
     std::cout << "from what number do you want to look: ";
     std::cin >> from;
     std::cout << "to what number do you want to look: ";
     std::cin >> to;
     std::cout << "you want to look for numbers divisible by: ";
     std::cin >> divider;
-
+    //edge cases
     if(to >= 2147483647 || from <= -2147483647){
         throw std::runtime_error("range is to big");
     }
     if(from > to){
         throw std::runtime_error("invalid range");
     }
-
+    //gating output
     for(int i = from; i <= to; i++){
 
         if(i % divider == 0){
-            std::cout << i << '\n';
+            std::cout << i << '\n'; //output
         }
     }
 
